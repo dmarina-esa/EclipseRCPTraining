7e4776fc986c9b7213a33ee86dd3ceac03dc1e4a
dmarina-esa <55544682+dmarina-esa@users.noreply.github.com>
2019-11-05 00:51:38 +0100

    correction

--- simulator/com.gmv.sportsimulator.api/src/com/gmv/sportsimulator/api/Game.java
+++ simulator/com.gmv.sportsimulator.api/src/com/gmv/sportsimulator/api/Game.java
@@ -355,12 +355,12 @@ public class Game
         if (teamScore1 > teamScore2)
         {
             getResult().finalise(teamScore1);
-            getTeam(teamScore1).addVictory();
+            getTeam(1).addVictory();
         }
         else if (teamScore2 > teamScore1)
         {
             getResult().finalise(teamScore2);
-            getTeam(teamScore2).addVictory();
+            getTeam(2).addVictory();
         }
         else
         {
